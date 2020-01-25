db3d2a96c51f260d676a3e73168bbe241e5ebfe8
Marcellin M. Wabo <5281242+mmarcwabo@users.noreply.github.com>
2020-01-25 13:26:50 +0100

    Update service/index.php
    Sanitize form

--- views/service/index.php
+++ views/service/index.php
@@ -7,26 +7,28 @@
 <input type="usern" name="telephone" id="telephone"><br/>
 <label for="email">Adresse mail :</label><br/>
 <input type="email" name="adressemail" id="email"><br/>
-<label for="role">Rôle :</label><br/>
+<label for="categorie">Choisir une catégorie :</label><br/>
 <select type="role" name="categorie" id="categorie">
     <option>Cyber Café</option>
     <option>Service de maintenance</option>
+    <option>Dépannage à domicile</option>
 </select><br/>
 <label for="adresse">Adresse :</label><br/>
 <textarea name="adresse" id="adresse"></textarea><br/>
 <label for="horairedisponibilite">Horaire de disponibilité :</label><br/>
 <input type="text" name="horairedisponibilite" id="horairedisponibilite"><br/>
-<label for="horairedisponibilite">Horaire de disponibilité :</label><br/>
-<input type="text" name="horairedisponibilite" id="horairedisponibilite"><br/>
-<label for="ville"></label><br/>
+<label for="ville">Ville : </label><br/>
 <select type="role" name="ville" id="ville">
     <option>Kinshasa</option>
     <option>Lubumbashi</option>
+    <option>Douala</option>
+    <option>Cotonou</option>
+    <option>Dakar</option>
 </select><br/>
 <label for="details">Details sur le service :</label><br/>
 <textarea name="details" id="details"></textarea><br/>
 <label></label><br/>
-<input type="submit" value="Ajouter utilisateur">
+<input type="submit" value="Reférencer Service">
 </form>
 <br/>
 <div id="listServices"></div>

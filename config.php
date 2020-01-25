502b995bdb73a196038926c001de37977d46e6c9
Marcellin M. Wabo <5281242+mmarcwabo@users.noreply.github.com>
2020-01-25 12:57:20 +0100

    Update config
    Add customisable params in config file

--- config.php
+++ config.php
@@ -4,15 +4,19 @@
 define('TITLE', 'Annuaires annexes'); 
 //App URL
 //Always provide a trailing slash (/) after a path
-define('URL', 'http://localhost/annuaires-sw/');
+//Feel these constant with your own values
+//Can need to update config in .htaccess too...or totally remove it
+define('URL', 'https://annuaires-sw.com/');
 //Libs path
 define('LIBS', 'libs/');
 //Hash keys
+//If auth needed, defin a HASH
 define('HASH_PASSWORD_KEY', '0fcdf4aba4b0f81f62d0ab2ed8');
 //Database configurations
-define('DBUSERNAME', 'root');
-define('DBPASSWORD', '');
-define('DBNAME', 'annuaires-sw');
+define('DBUSERNAME', 'user');
+define('DBPASSWORD', 'password');
+define('DBNAME', 'test');
+//Your database type
 define('DBTYPE', 'mysql');
-define('DBHOST', 'localhost');
+define('DBHOST', 'database hostname');
 
